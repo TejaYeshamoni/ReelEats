31f768a4860e01d274e27da871926db3d14abdfa
TejaYeshamoni <yteja01@gmail.com>
2025-12-01 17:34:13 +0530

    Add MongoDB connection and update dependencies
    - Implemented MongoDB connection in db.js
    - Updated package.json and package-lock.json to include mongoose dependency
    - Modified app.js response message

---
+++ backend/src/db/db.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose')
+
+
+function connectDB(){
+    mongoose.connect('mongodb://localhost:27017/reeleats')
+    .then(() => {
+        console.log('Connected to MongoDB');
+    })
+    .catch((err) => {
+        console.error('Error connecting to MongoDB:', err);
+    })
+}
+
+module.exports = connectDB;
