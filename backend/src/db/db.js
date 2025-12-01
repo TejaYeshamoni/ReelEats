59387a0ea04c793859799267f9a5821692f5448e
TejaYeshamoni <yteja01@gmail.com>
2025-12-01 20:29:41 +0530

    Implement user authentication with registration and login features

--- backend/src/db/db.js
+++ backend/src/db/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 
 function connectDB(){
-    mongoose.connect('mongodb://localhost:27017/reeleats')
+    mongoose.connect(process.env.MONGODB_URI)
     .then(() => {
         console.log('Connected to MongoDB');
     })
